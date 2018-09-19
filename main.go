6de9a20926763a29ea54768412d1300c4ef9cc32
vworri <vworri@gmail.com>
2018-09-18 23:43:26 -0400

    got termui to put up a list but wanna try gocui

--- main.go
+++ main.go
@@ -4,10 +4,32 @@ import (
 	"fmt"
 
 	aux "github.com/GoEavesDrop/Aux"
+	ui "github.com/gizak/termui" // <- ui shortcut, optional
 )
 
 func main() {
 	//find all devices
 	var NetInfo = aux.GetNetworkDeviceInfo()
-	fmt.Print(NetInfo)
+	if err := ui.Init(); err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+	var menu []string
+	for i, netInt := range NetInfo {
+		menu = append(menu, fmt.Sprintf("[%d] %s\n", i, netInt.Name))
+	}
+	ls := ui.NewList()
+	ls.Items = menu
+	ls.ItemFgColor = ui.ColorYellow
+	ls.BorderLabel = "List"
+	ls.Height = 7
+	ls.Width = 25
+	ls.Y = 0
+
+	ui.Render(ls)
+	ui.Handle("q", func(ui.Event) {
+		// press q to quit
+		ui.StopLoop()
+	})
+	ui.Loop()
 }
